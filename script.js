0d060b453b28ebbeb34a93e744c6151a29a57580
Paula Fenner <fenner.paula@gmail.com>
2024-06-19 14:26:44 +0200

    revisited JS exercise

--- script.js
+++ script.js
@@ -1,61 +1,40 @@
-let displayHomeEl = document.getElementById('display-home');
-let displayGuestEl = document.getElementById('display-guest');
-let resetBtnEl = document.getElementById('reset-btn');
+const displayHomeEl = document.getElementById('display-home');
+const displayGuestEl = document.getElementById('display-guest');
+const resetBtnEl = document.getElementById('reset-btn');
 
-let home1Btn = document.getElementById('home-1-btn');
-let home2Btn = document.getElementById('home-2-btn');
-let home3Btn = document.getElementById('home-3-btn');
+const home1Btn = document.getElementById('home-1-btn');
+const home2Btn = document.getElementById('home-2-btn');
+const home3Btn = document.getElementById('home-3-btn');
 
-let guest1Btn = document.getElementById('guest-1-btn');
-let guest2Btn = document.getElementById('guest-2-btn');
-let guest3Btn = document.getElementById('guest-3-btn');
+const guest1Btn = document.getElementById('guest-1-btn');
+const guest2Btn = document.getElementById('guest-2-btn');
+const guest3Btn = document.getElementById('guest-3-btn');
 
 let homeTeam = 0;
 let guestTeam = 0;
 
-home1Btn.addEventListener('click', add1Home);
-home2Btn.addEventListener('click', add2Home);
-home3Btn.addEventListener('click', add3Home);
+// Comments on the README
+home1Btn.addEventListener('click', () => addPointsToHomeTeam(1));
+home2Btn.addEventListener('click', () => addPointsToHomeTeam(2));
+home3Btn.addEventListener('click', () => addPointsToHomeTeam(3));
 
-guest1Btn.addEventListener('click', add1Guest);
-guest2Btn.addEventListener('click', add2Guest);
-guest3Btn.addEventListener('click', add3Guest);
+guest1Btn.addEventListener('click', () => addPointsToGuestTeam(1));
+guest2Btn.addEventListener('click', () => addPointsToGuestTeam(2));
+guest3Btn.addEventListener('click', () => addPointsToGuestTeam(3));
 
 resetBtnEl.addEventListener('click', clearAll);
 
-function add1Home() {
-  let homeScore = (homeTeam += 1);
-  displayHomeEl.innerText = homeScore;
+function addPointsToHomeTeam(points) {
+  displayHomeEl.innerText = homeTeam += points;
 }
 
-function add2Home() {
-  let homeScore = (homeTeam += 2);
-  displayHomeEl.innerText = homeScore;
-}
-
-function add3Home() {
-  let homeScore = (homeTeam += 3);
-  displayHomeEl.innerText = homeScore;
-}
-
-function add1Guest() {
-  let guestScore = (guestTeam += 1);
-  displayGuestEl.innerText = guestScore;
-}
-
-function add2Guest() {
-  let guestScore = (guestTeam += 2);
-  displayGuestEl.innerText = guestScore;
-}
-
-function add3Guest() {
-  let guestScore = (guestTeam += 3);
-  displayGuestEl.innerText = guestScore;
+function addPointsToGuestTeam(points) {
+  displayGuestEl.innerText = guestTeam += points;
 }
 
 function clearAll() {
   homeTeam = 0;
   guestTeam = 0;
-  displayHomeEl.innerText = '';
-  displayGuestEl.innerText = '';
+  displayHomeEl.innerText = 0;
+  displayGuestEl.innerText = 0;
 }
